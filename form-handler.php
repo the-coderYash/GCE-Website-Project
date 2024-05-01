21d7682ff0f70fc57658df547942a2d25c97ad9e
Yash Raj <yashraj00112@gmail.com>
2024-05-02 00:52:55 +0530

    //Hosted my website on GItHub

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@gcegaya.ac.in';
+$email_from = 'noreply@the-coderyash.github.io';
 
 $email_subject = 'New Form Submission';
 
